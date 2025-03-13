a92298d80864e5ef08be4905964450cf6a43d61c
AlessandroSottosanti <asottosanti2@gmail.com>
2025-03-13 16:01:35 +0100

    feat: bonus

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -10,32 +10,52 @@ public class CheckGuest {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Inserire il proprio nome completo");
-        String fullName = sc.nextLine().toLowerCase().trim();
+        String fullName = sc.nextLine().trim().replaceAll("\\s+", " ").toLowerCase();
 
         Boolean isInvited = false;
 
-        for (int i = 0; i < arrayInvitati.length; i++) {
-            String invitatoCorrente = arrayInvitati[i].trim().toLowerCase();
+        // for (int i = 0; i < arrayInvitati.length; i++) {
+        // String invitatoCorrente = arrayInvitati[i].trim().replaceAll("\\s+", "
+        // ").toLowerCase();
 
-            String nomeNormalizzato = invitatoCorrente.trim().replaceAll("\\s+", " ").toLowerCase();
-            String fullNameNormalizzato = fullName.trim().replaceAll("\\s+", " ").toLowerCase();
 
-            boolean isHim = nomeNormalizzato.equals(fullNameNormalizzato);
+        // boolean isHim = invitatoCorrente.equals(fullName);
 
-            // System.out.println(nomeNormalizzato + " == " + fullNameNormalizzato + " : " + isHim);
+        // // System.out.println(invitatoCorrente + " == " + fullName + " :
+        // " + isHim);
+
+        // if (isHim) {
+        // isInvited = true;
+        // System.out.println("Benvenuto alla festa dei Ferragnez " +
+        // fullName + "!");
+        // break;
+        // }
+
+        // }
+        
+
+        int i = 0;
+
+        boolean isHim = false;
+
+        while (!isInvited && !isHim && i < arrayInvitati.length) {
+
+            String invitatoCorrente = arrayInvitati[i].trim().replaceAll("\\s+", " ").toLowerCase();
+
+            isHim = invitatoCorrente.equals(fullName);
+
+             System.out.println(invitatoCorrente + " == " + fullName + " : " + isHim);
 
             if (isHim) {
                 isInvited = true;
-                System.out.println("Benvenuto alla festa dei Ferragnez " + fullNameNormalizzato + "!");
-                break;
+                System.out.println("Benvenuto alla festa dei Ferragnez " + fullName + "!");
             }
 
+            i++;
         }
 
         if (!isInvited) {
-
             System.out.println("torna a casa tua ciccio/a :)");
         }
-
     }
 }
